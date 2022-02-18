8d26a47003d9403fca7ab18f480db0a0138251c0
ada <304368809@qq.com>
2022-02-18 22:32:42 +0800

    使用v-model数据

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-    <Switch />
+    <Switch  v-model:value="y" />
   </div>
 </template>
 
 <script lang="ts">
 import Switch from  '../lib/Switch.vue'
+import { ref } from 'vue'
 export default {
-  name: 'SwitchDemo',
-  components: {Switch}
+  components: {Switch},
+  props:{
+    value: Boolean,
+  },
+  setup(){
+    const y = ref(false)
+    return {y}
+  }
 };
 </script>
 <style lang="scss" scoped>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -36,6 +36,7 @@ span{
   width: $h2;
   background: white;
   border-radius: $h2 /2;
+  transition: left 250ms;
 }
 button.checked{
   background: blue;
