908a896f39180b7f0e8b9747b2bfde2f98c477b3
ada <304368809@qq.com>
2022-02-19 17:36:12 +0800

    完成dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,31 @@
 <template>
-<div>dialog的文档</div>
+  <div>Dialog示例</div>
+  <h1>示例1</h1>
+  <Button @click="toggle">toggle</Button>
+<Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">dialog组件</Dialog>
 </template>
 
 <script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {ref} from 'vue';
 export default {
-  name: 'DialogDemo'
+  name: 'DialogDemo',
+  components:{Button, Dialog},
+  setup(){
+    const x=ref(false)
+    const toggle=()=>{
+      x.value=!x.value
+    }
+    const f1 = () => {
+
+      return false
+    }
+    const f2 = () => {
+
+    }
+    return {x,toggle,f1,f2}
+  }
 };
 </script>
 
