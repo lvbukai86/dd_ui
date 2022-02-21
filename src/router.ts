c7984455d5c9f8ae50ec1dbea665c7769199b684
lvbukai86 <304368809@qq.com>
2022-02-21 16:59:24 +0800

    删除无用代码

--- src/router.ts
+++ src/router.ts
@@ -7,9 +7,6 @@ import DocDemo from './components/DocDemo.vue';
 import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
-import Intro from './views/Intro.vue';
-import Install from './views/Install.vue';
-import GetStarted from './views/GetStarted.vue';
 import {h} from 'vue'
 import MakeDown from './components/Makedown.vue';
 const history = createWebHashHistory();
